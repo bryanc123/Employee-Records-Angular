a0ab075914781d2e511b1cb214398aaf7e623a86
Bryan Cayanan <cayanan.bryanau@gmail.com>
2021-05-22 11:51:58 -0700

    Added built-in validators to name and email fields

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from '../user.service';
+import { User } from '../user';
 
 @Component({
   selector: 'app-add-user',
@@ -9,16 +11,19 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class AddUserComponent implements OnInit {
 
   userForm = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('', Validators.required),
     username: new FormControl(''),
-    email: new FormControl(''),
+    email: new FormControl('', Validators.email),
     address: new FormGroup({
       street: new FormControl(''),
       suite: new FormControl(''),
       city: new FormControl(''),
       zipcode: new FormControl(''),
+      lat: new FormControl(''),
+      lng: new FormControl('')
     }),
     phone: new FormControl(''),
+    website: new FormControl(''),
     company: new FormGroup({
       name: new FormControl(''),
       catchPhrase: new FormControl(''),
@@ -26,9 +31,42 @@ export class AddUserComponent implements OnInit {
     })
   });
   
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+  }
+
+  get name() {
+    return this.userForm.get('name');
+  }
+
+  get email() {
+    return this.userForm.get('email');
+  }
+
+  onSubmit() {
+    this.userService.postUser({
+      id: 20,
+      name: this.userForm.get('name').value,
+      username: this.userForm.get('username').value,
+      email: this.userForm.get('email').value,
+      address: {
+        street: this.userForm.get('address.street').value,
+        suite: this.userForm.get('address.suite').value,
+        city: this.userForm.get('address.city').value,
+        zipcode: this.userForm.get('address.zipcode').value,
+        geo: {
+          lat: this.userForm.get('address.lat').value,
+          lng: this.userForm.get('address.lng').value,
+        }
+      },
+      phone: this.userForm.get('phone').value,
+      website: this.userForm.get('website').value,
+      company: this.userForm.get('company').value
+    }).subscribe(user => {
+      console.log(this.userForm.value);
+      //this.userForm.reset();
+    });
   }
 
 }
